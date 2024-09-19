ee63f87555d7f7d7826ee20aba99fb84819fbd05
YurikoAishinselo123 <aishinseloyuriko124@gmail.com>
2024-09-19 09:03:39 +0700

    Camera Follow

--- Assets/Script/Player/CameraFollow.cs
+++ Assets/Script/Player/CameraFollow.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target; 
+    public Vector3 offset;   
+    public float smoothSpeed = 0.225f; 
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
     }
 }
