ab4d201a8d220d6f67c937a3027f160a11c4cf38
YurikoAishinselo123 <aishinseloyuriko124@gmail.com>
2024-09-20 13:41:40 +0700

    fix bug, and Add Feature
    Clear Folder Name
    Fix Jumping Bug
    Platform D

--- Assets/Script/Player/S_Tanko/TankoController.cs
+++ Assets/Script/Player/S_Tanko/TankoController.cs
@@ -1,4 +1,6 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class TankoController : MonoBehaviour
 {
@@ -6,9 +8,11 @@ public class TankoController : MonoBehaviour
     [SerializeField] private float jumpForce = 5f;
     public float horizontalAxis;
     private Vector2 direction;
+    private int jumpLeft = 1;
 
     [SerializeField] private Rigidbody2D rb;
     // [SerializeField] private Animator animator;
+
 
     void Start()
     {
@@ -22,7 +26,6 @@ public class TankoController : MonoBehaviour
         Jump();
         // Facing();
     }
-
 
     void Movement()
     {
@@ -47,10 +50,11 @@ public class TankoController : MonoBehaviour
 
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && jumpLeft > 0)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             // animator.SetTrigger("Jump");
+            jumpLeft = jumpLeft-1;
         }
     }
 
@@ -65,4 +69,14 @@ public class TankoController : MonoBehaviour
             transform.localScale = new Vector3(5, 5, 5);
         }
     }
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            jumpLeft = 1;
+        }
+    }
+
+
 }
+
