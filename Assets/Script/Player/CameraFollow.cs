04fdcd1a97e74f71c8bf28d39f46f307d53b641c
YurikoAishinselo123 <aishinseloyuriko124@gmail.com>
2024-09-27 20:53:44 +0700

    Feat/mobile controller (#38)
    * Button Mobile Controller
    
    * Pull from dev and push to this branch
    
    * Fix Canvas to Scale with Screen size
    
    * Add Button Controller in Canvas
    
    * Fix Scene Bustling City
    
    * Fix Scene
    
    * Fix Jump Button Controller

--- Assets/Script/Player/CameraFollow.cs
+++ Assets/Script/Player/CameraFollow.cs
@@ -1,16 +1,18 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Netcode;  
 
 public class CameraController : MonoBehaviour
 {
     Transform target;
+
     Vector3 velocity = Vector3.zero;
 
-    [Range(0, 1)]
+    [Range(0,1)]
     public float smoothTime;
-    public Vector3 cameraOffset;
+
+    public Vector3 cameraOfset;
 
     private void Awake() {
         StartCoroutine(FindPlayer());
@@ -18,28 +20,14 @@ public class CameraController : MonoBehaviour
 
     IEnumerator FindPlayer()
     {
-        // Check if this is the host or client
-        bool isHost = NetworkManager.Singleton.IsHost;
-        
         // Mencoba mencari player hingga ditemukan
         while (target == null)
         {
-            GameObject player = null;
-
-            if (isHost)
-            {
-                player = GameObject.FindGameObjectWithTag("Tanko");  // Assuming Tanko is tagged as "Host"
-            }
-            else
-            {
-                player = GameObject.FindGameObjectWithTag("Gaspi");  // Assuming Gaspi is tagged as "Client"
-            }
-
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
             if (player != null)
             {
                 target = player.transform;
             }
-
             // Tunggu satu frame sebelum mencoba lagi
             yield return null;
         }
@@ -48,7 +36,7 @@ public class CameraController : MonoBehaviour
     private void LateUpdate() {
         if (target == null) return;
 
-        Vector3 targetPosition = target.position + cameraOffset;
+        Vector3 targetPosition = target.position + cameraOfset;
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
