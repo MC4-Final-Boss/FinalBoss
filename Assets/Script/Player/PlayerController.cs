fb47a1627cf1c771f955474b8739e00563409604
YurikoAishinselo123 <aishinseloyuriko124@gmail.com>
2024-10-02 10:53:37 +0700

    Player Animation

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -3,34 +3,42 @@ using Unity.Netcode;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System;
+using System.Collections;
 
 public class PlayerController : NetworkBehaviour
 {
-    [SerializeField] private float movementSpeed = 7f;
-    [SerializeField] private float jumpForce = 3f;
-    [SerializeField] private int jumpLeft = 1;
-    [SerializeField] private int pressedPlayer = 0;
-    [SerializeField] private float fallThreshold = -15f;
     private SFXManager sfxManager;
-
     [SerializeField] private float jumpVelocityThreshold = 0.1f;
 
     private Button leftButton;
     private Button rightButton;
     private Button jumpButton;
 
+    [SerializeField] private float movementSpeed = 7f;
+    [SerializeField] private float jumpForce = 3f;
+    [SerializeField] private bool isGround = true;
+
+    [SerializeField] private int pressedPlayer = 0;
+    [SerializeField] private bool explodePlayer = false;
+    [SerializeField] private bool drown = false;
+    [SerializeField] private float fallThreshold = -15f;
+
     [SerializeField] private Rigidbody2D rb;
     private Animator animator;
     private Vector3 movement;
 
     private NetworkVariable<Vector2> netPosition = new NetworkVariable<Vector2>();
-    private NetworkVariable<bool> netFacingRight = new NetworkVariable<bool>(true);
+    private NetworkVariable<float> netFacingDirection = new NetworkVariable<float>();
+    private NetworkVariable<float> netAnimationMoving = new NetworkVariable<float>();
+    private NetworkVariable<bool> netExplodePlayer = new NetworkVariable<bool>();
+    private NetworkVariable<bool> netDrown = new NetworkVariable<bool>();
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         sfxManager = FindObjectOfType<SFXManager>();
+        GetComponent<Rigidbody2D>().isKinematic = false;
         leftButton = GameObject.Find("Left Button").GetComponent<Button>();
         rightButton = GameObject.Find("Right Button").GetComponent<Button>();
         jumpButton = GameObject.Find("Jump Button").GetComponent<Button>();
@@ -38,6 +46,7 @@ public class PlayerController : NetworkBehaviour
         if (IsOwner)
         {
             SetupButtons();
+            netFacingDirection.Value = transform.localScale.x;
         }
     }
 
@@ -61,6 +70,7 @@ public class PlayerController : NetworkBehaviour
         else
         {
             rb.MovePosition(netPosition.Value);
+            transform.localScale = new Vector3(netFacingDirection.Value, transform.localScale.y, transform.localScale.z);
         }
     }
 
@@ -70,13 +80,16 @@ public class PlayerController : NetworkBehaviour
         {
             Facing();
             Animations();
-            // HandleInput();
+            HandleInput();
             UpdatePositionServerRpc(rb.position);
+            UpdateAnimationMovingServerRpc(Mathf.Abs(movement.x));
+            UpdateAnimationBoolsServerRpc(explodePlayer, drown);
         }
         else
         {
-            // Update local facing based on network variable
-            transform.localScale = new Vector3(netFacingRight.Value ? Mathf.Abs(transform.localScale.x) : -Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            animator.SetFloat("Moving", netAnimationMoving.Value);
+            animator.SetBool("ExplodePlayer", netExplodePlayer.Value);
+            animator.SetBool("Drown", netDrown.Value);
         }
     }
 
@@ -89,7 +102,7 @@ public class PlayerController : NetworkBehaviour
         else
             movement.x = 0f;
 
-        if (Input.GetKeyDown(KeyCode.I) && jumpLeft > 0)
+        if (Input.GetKeyDown(KeyCode.I) && isGround)
             Jump();
     }
 
@@ -116,10 +129,10 @@ public class PlayerController : NetworkBehaviour
 
     public void Jump()
     {
-        if (IsOwner && pressedPlayer == 0 && jumpLeft > 0)
+        if (IsOwner && pressedPlayer == 0 && isGround)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            jumpLeft--;
+            isGround = false;
 
             // Play jumping sound
             if (sfxManager != null)
@@ -134,63 +147,147 @@ public class PlayerController : NetworkBehaviour
         bool isJumping = Mathf.Abs(rb.velocity.y) > jumpVelocityThreshold;
         animator.SetFloat("Moving", isJumping ? 0 : Mathf.Abs(movement.x));
         animator.SetBool("IsJumping", isJumping);
+        animator.SetBool("ExplodePlayer", explodePlayer);
+        animator.SetBool("Drown", drown);
     }
 
     void Facing()
     {
         if (movement.x != 0)
         {
-            bool isFacingRight = movement.x > 0;
-            transform.localScale = new Vector3(isFacingRight ? Mathf.Abs(transform.localScale.x) : -Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            UpdateFacingServerRpc(isFacingRight);
+            // Determine the new facing direction based on movement direction
+            float newFacingDirection = Mathf.Sign(movement.x) * Mathf.Abs(transform.localScale.x);
+            transform.localScale = new Vector3(newFacingDirection, transform.localScale.y, transform.localScale.z);
+
+            // Update facing direction on the server
+            UpdateFacingServerRpc(newFacingDirection);
         }
     }
 
     [ServerRpc]
-    private void UpdateFacingServerRpc(bool isFacingRight)
+    private void UpdateFacingServerRpc(float newFacingDirection)
     {
-        netFacingRight.Value = isFacingRight;
+        netFacingDirection.Value = newFacingDirection;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (IsOwner)
         {
-            if (!other.gameObject.CompareTag("Tanko") && !other.gameObject.CompareTag("Gaspi"))
+            PlayerRespawn respawnScript = GetComponent<PlayerRespawn>();
+
+            // Memeriksa kecepatan jatuh
+            if (rb.velocity.y <= fallThreshold)
             {
-                jumpLeft = 1;
-            }
-            else if (other.gameObject.CompareTag("Tanko") || other.gameObject.CompareTag("Gaspi"))
-            {
-                if (transform.position.y < other.transform.position.y)
+                if (respawnScript != null)
                 {
-                    jumpLeft = 0;
-                    pressedPlayer = 1;
+                    // Jalankan Coroutine untuk ledakan dan respawn
+                    StartCoroutine(HandleExplosionAndRespawn(respawnScript));
+                }
+                return;
+            }
+
+            // if (other.gameObject.CompareTag("Water"))
+            // {
+            //     // Debug: Pastikan OnTriggerEnter mendeteksi air
+            //     Debug.Log("Player touched water, starting HandleDrownAndRespawn...");
+            //     StartCoroutine(HandleDrownAndRespawn(respawnScript));
+            // }
+
+            if (other.gameObject.CompareTag("Tanko") || other.gameObject.CompareTag("Gaspi"))
+            {
+                if (other.transform.position.y > transform.position.y)
+                {
+                    isGround = false;
+                    drown = true;
+                    Debug.Log("Ada player diatasnya");
                 }
                 else
                 {
-                    jumpLeft = 1;
+                    isGround = true;
+
                 }
             }
-
-            if (rb.velocity.y <= fallThreshold)
+            else // Jika objek lain bukan Gaspi atau Tanko
             {
-                PlayerRespawn respawnScript = GetComponent<PlayerRespawn>();
-                if (respawnScript != null)
+                // Jika berada di atas objek ini
+                if (other.transform.position.y > transform.position.y && other.gameObject.CompareTag("BasicBox"))
                 {
-                    respawnScript.RespawnPlayer();
+                    if (respawnScript != null)
+                    {
+                        // Jalankan Coroutine untuk ledakan dan respawn
+                        StartCoroutine(HandleExplosionAndRespawn(respawnScript));
+                        Debug.Log("Ada sesuatu diatasnya");
+                    }
+                    return;
+                }
+                else
+                {
+                    isGround = true;
+
                 }
             }
         }
+    }
+
+
+    // Coroutine untuk menangani ledakan dan respawn ketika player jatuh terlalu jauh
+    IEnumerator HandleExplosionAndRespawn(PlayerRespawn respawnScript)
+    {
+        explodePlayer = true; // Aktifkan animasi ledakan
+
+        // Tunggu durasi animasi ledakan
+        AnimatorStateInfo animStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        float animLength = animStateInfo.length;
+        float customDuration = animLength * 1f; // Ganti durasi jika perlu
+        yield return new WaitForSeconds(customDuration); // Tunggu sesuai durasi kustom
+
+        // Reset explodePlayer setelah animasi selesai
+        explodePlayer = false;
+
+        // Respawn player setelah animasi selesai
+        respawnScript.RespawnPlayer();
+        Debug.Log("Player Death and Respawned");
+    }
+
+
+    IEnumerator HandleDrownAndRespawn(PlayerRespawn respawnScript)
+    {
+        drown = true; // Aktifkan animasi tenggelam
+
+        yield return new WaitForSeconds(1f); // Tunggu 2 detik
+
+        // Respawn player setelah durasi
+        respawnScript.RespawnPlayer();
+        Debug.Log("Player Death and Respawned");
+
+        // Reset drown
+        drown = false;
+
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         if ((other.gameObject.CompareTag("Tanko") || other.gameObject.CompareTag("Gaspi")))
         {
-            jumpLeft = 1;
+            isGround = true;
             pressedPlayer = 0;
+            drown = false;
+
         }
+    }
+
+    [ServerRpc]
+    private void UpdateAnimationMovingServerRpc(float newMovingValue)
+    {
+        netAnimationMoving.Value = newMovingValue;
+    }
+
+    [ServerRpc]
+    private void UpdateAnimationBoolsServerRpc(bool newExplodePlayer, bool newDrown)
+    {
+        netExplodePlayer.Value = newExplodePlayer;
+        netDrown.Value = newDrown;
     }
 
     [ServerRpc]
