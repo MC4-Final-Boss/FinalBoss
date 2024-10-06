34e0221075bc187f607ed5328396eb59cf99dfbe
M N Ikhsan Mubaarak <126537753+baarak08@users.noreply.github.com>
2024-10-06 22:44:28 +0700

    updating New Bustling City Scene after water animation

---
+++ Assets/Script/Obstacles/ButtonGaspi.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class ButtonGaspi : MonoBehaviour
+{
+    public GameObject pressedButton;
+    public GameObject movementGround;
+    public Vector3 targetPosition;
+    public float moveSpeed = 2f;
+
+    private Vector3 initialPosition;
+    private bool platformMove = false;
+    private bool buttonActive = false;
+
+    private SFXManager sfxManager; 
+
+    private void Start()
+    {
+        initialPosition = movementGround.transform.localPosition;  
+         sfxManager = FindObjectOfType<SFXManager>();
+    }
+
+    private void Update()
+    {
+        if (platformMove)
+        {
+            Vector3 targetPos = buttonActive ? targetPosition : initialPosition;
+
+            // Debug.Log("target position: " + targetPosition);
+            movementGround.transform.localPosition = Vector3.MoveTowards(
+                movementGround.transform.localPosition,  
+                targetPos,
+                moveSpeed * Time.deltaTime
+            );
+
+            if (Vector3.Distance(movementGround.transform.localPosition, targetPos) < 0.01f)
+            {
+                platformMove = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Gaspi") || other.gameObject.CompareTag("BasicBox"))
+        {
+            Debug.Log("Touch Button");
+            platformMove = true;
+            buttonActive = true;
+        }
+
+        // Play button press sound
+            if (sfxManager != null)
+            {
+                sfxManager.PlayButtonPressSFX();
+            }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Gaspi") || other.gameObject.CompareTag("BasicBox"))
+        {
+            Debug.Log("Tanko left the button");
+            platformMove = true;
+            buttonActive = false;
+        }
+    }
+}
